4f36867ea10c033c435a185632700279deae22be
k2works <kakimomokuri@gmail.com>
2017-02-09 12:27:34 +0900

    Step04

--- test/FrameTest.cs
+++ test/FrameTest.cs
@@ -12,5 +12,13 @@ namespace test
             Frame f = new Frame();
             Assert.AreEqual(0, f.Score);
         }
+
+        [Test]
+        public void TestAddOneThrow()
+        {
+            Frame f = new Frame();
+            f.Add(5);
+            Assert.AreEqual(5, f.Score);
+        }
     }
 }
