a537cf4e6b08bb4e846ca686a90001bcb71c7d9f
k2works <kakimomokuri@gmail.com>
2017-02-09 12:30:11 +0900

    Step05

--- lib/Frame.cs
+++ lib/Frame.cs
@@ -1,10 +1,16 @@
-﻿namespace lib
+﻿using System;
+
+namespace lib
 {
     public class Frame
     {
         public int Score
         {
             get { return 0; }
+        }
+
+        public void Add(Throw t)
+        {
         }
     }
 }
