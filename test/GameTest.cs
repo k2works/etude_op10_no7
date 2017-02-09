a6f865c4805fbca399443ed245705cca7d448ce3
k2works <kakimomokuri@gmail.com>
2017-02-09 12:41:11 +0900

    Step08

---
+++ test/GameTest.cs
@@ -0,0 +1,18 @@
+﻿using lib;
+using NUnit.Framework;
+
+namespace test
+{
+    [TestFixture]
+    public class GameTest
+    {
+        [Test]
+        public void TestOneThrow()
+        {
+            Game game = new Game();
+
+            game.Add(5);
+            Assert.AreEqual(5, game.Score);
+        }
+    }
+}
