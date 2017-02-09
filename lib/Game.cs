35cc9d36e9bbed8558e7ad6c61f8896e0597378b
k2works <kakimomokuri@gmail.com>
2017-02-09 12:47:51 +0900

    Step09

---
+++ lib/Game.cs
@@ -0,0 +1,14 @@
+﻿namespace lib
+{
+    public class Game
+    {
+        public int Score
+        {
+            get { return 0; }
+        }
+
+        public void Add(int pins)
+        {
+        }
+    }
+}
