352e6ff03680d8674c3fc81cd67a7133326949a7
k2works <kakimomokuri@gmail.com>
2017-02-09 12:33:33 +0900

    Step07

--- lib/Frame.cs
+++ lib/Frame.cs
@@ -4,13 +4,16 @@ namespace lib
 {
     public class Frame
     {
+        private int score;
+
         public int Score
         {
-            get { return 0; }
+            get { return score; }
         }
 
         public void Add(int pins)
         {
+            score += pins;
         }
     }
 }
