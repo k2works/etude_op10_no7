b66a8995ee614b436debb7b8f4f7dfc7a56e51b2
k2works <kakimomokuri@gmail.com>
2017-02-11 11:54:23 +0900

    Step25

--- test/GameTest.cs
+++ test/GameTest.cs
@@ -27,11 +27,8 @@ namespace test
         {
             game.Add(5);
             game.Add(4);
-            game.Add(7);
-            game.Add(2);
-            Assert.AreEqual(18, game.Score);
             Assert.AreEqual(9, game.ScoreForFrame(1));
-            Assert.AreEqual(18, game.ScoreForFrame(2));
+            Assert.AreEqual(1, game.CurrentFrame);
         }
 
         [Test]
