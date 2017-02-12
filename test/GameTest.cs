0b3d447634e02ee4fbfec6675ad1a755ea1488a9
k2works <kakimomokuri@gmail.com>
2017-02-12 12:57:26 +0900

    Step39

--- test/GameTest.cs
+++ test/GameTest.cs
@@ -82,7 +82,7 @@ namespace test
                 game.Add(10);
             }
             Assert.AreEqual(300, game.Score());
-            Assert.AreEqual(10, game.CurrentFrame);
+            Assert.AreEqual(11, game.CurrentFrame);
         }
     }
 }
